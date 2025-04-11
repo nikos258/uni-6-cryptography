e59a0fd6d40e35992e72310bd642fd4f56ad16e2
nikos258 <nikar.nikar10@gmail.com>
2025-04-11 16:43:10 +0300

    update ask1

--- ask1.py
+++ ask1.py
@@ -1,33 +1,44 @@
-import math
-import numpy as np
+# 2.2 (19)
 
 
-g = np.poly1d([1, 3, 1])
+def prepare_dictionaries():
+    """
+    Makes two dictionaries to join the letters of the greek alphabet and numbers 1-24.
+    d1 = {'α':1, 'β':2, ...}
+    d2 = {1:'α', 2:'β', ...}
+    :return: the two dictionaries
+    """
+    d1 = dict()
+    d2 = dict()
+    n = ord('α')
+    for i in range(17):
+        d1[chr(n)] = i+1
+        d2[i+1] = chr(n)
+        n += 1
 
-f = np.poly1d([1, 3, 3, 7, 5, 4])
-
-d1 = dict()
-d2 = dict()
-n = ord('α')
-for i in range(17):
-    d1[chr(n)] = i+1
-    d2[i+1] = chr(n)
     n += 1
+    for i in range(17, 24):
+        d1[chr(n)] = i+1
+        d2[i+1] = chr(n)
+        n += 1
 
-n += 1
-for i in range(17, 24):
-    d1[chr(n)] = i+1
-    d2[i+1] = chr(n)
-    n += 1
+    return d1, d2
 
-cipher = "οκηθμφδζθγοθχυκχσφθμφμχγ"
-message = ""
-for letter in cipher:
-    idx = d1[letter] - 3
-    if idx < 1:
-        message += d2[(idx-1) % 24 +1]
-    else:
-        message += d2[idx]
 
-print(message)
-# print(d1, d2, sep="\n")
+def main():
+    letter_num, num_letter = prepare_dictionaries()
+
+    cipher = "οκηθμφδζθγοθχυκχσφθμφμχγ"
+    message = ""
+    for letter in cipher:
+        idx = letter_num[letter] - 3  # subtract f(x0), where x0 is a root of g
+        if idx < 1:  # handles the case when the index is not in the range [1, 24]
+            message += num_letter[(idx-1) % 24 + 1]
+        else:
+            message += num_letter[idx]
+
+    print("The original message is:", message, sep='\n')
+
+
+if __name__ == '__main__':
+    main()
