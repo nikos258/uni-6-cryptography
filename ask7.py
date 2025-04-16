6194a41f2ef66bc67572ee3979730d5a7dd0d12b
nikos258 <nikar.nikar10@gmail.com>
2025-04-16 18:19:17 +0300

    update ask7

--- ask7.py
+++ ask7.py
@@ -5,7 +5,12 @@ import ask4
 n = 2**5
 
 
-def gen_permutation(seed):
+def gen_permutation(seed: list[int]) -> list[int]:
+    """
+    Generates the permutation s for the RC4 cryptography algorithm.
+    :param seed: a list with 40 up to 256 bits
+    :return: the initial permutation s
+    """
     seedlen = len(seed)
     s = [i for i in range(n)]
 
@@ -18,9 +23,15 @@ def gen_permutation(seed):
     return s
 
 
-def gen_keystream(s, plaintext):
+def gen_keystream(s, plen):
+    """
+    Generates the keystream for the RC4 cryptography algorithm.
+    :param s: the initial permutation
+    :param plen:
+    :return: the whole keystream
+    """
     i = j = 0
-    plen = len(plaintext)
+
     k = []
     while plen > 0:
         i = (i + 1) % n
@@ -34,25 +45,25 @@ def gen_keystream(s, plaintext):
 
 
 message = "MISTAKES ARE AS SERIOUS AS THE RESULTS THEY CAUSE".replace(' ', '')
+print("message:", message)
 key = "HOUSE"
+print("key:", key)
 
 message_bits = ask4.encode(message)
+print("message bits:", message_bits)
+
 key_bits = [int(bit) for bit in ask4.encode(key)]
-
 permutation = gen_permutation(key_bits)
-# keystream = gen_keystream(permutation, message)
-# print(len(message), len(keystream), sep='\n')
-key_stream = "".join(gen_keystream(permutation, message))
 
-print("key stream: ", key_stream)
+key_stream = "".join(gen_keystream(permutation, len(message)))
+print("key stream:", key_stream)
 
-cipher = ask4.encrypt(key_stream, message_bits)
-print("cipher: ", cipher)
+cipher_bits = ask4.encrypt(key_stream, message_bits)
+print("cipher bits:", cipher_bits)
 
-decrypted_cipher = ask4.decrypt(key_stream, cipher)
+ciphertext = ask4.decode(cipher_bits)
+print("ciphertext:", ciphertext)
+
+decrypted_cipher = ask4.decrypt(key_stream, cipher_bits)
 decrypted_ciphertext = ask4.decode(decrypted_cipher)
-print(decrypted_ciphertext)
-
-
-
-
+print("decrypted ciphertext:", decrypted_ciphertext)
