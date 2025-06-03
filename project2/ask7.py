84d1e61292d3f79fdb9b7710cc8bef503d20b5d4
nikos258 <nikar.nikar10@gmail.com>
2025-06-03 22:13:35 +0300

    update ask7

--- project2/ask7.py
+++ project2/ask7.py
@@ -3,15 +3,27 @@ import random
 
 
 def fermat_test(n):
+    """
+    Implements Fermat's Test of primality
+    :param n: a positive integer > 2
+    :return: True if the number is composite, False if the number is probably prime
+    """
     a = random.randint(2, n-1)
     if gcd(n, a) > 1:
         return True
-    if a**(n-1) % n == 1:
+    if pow(a, n-1, n) == 1:
         return False
     return True
 
 
 def gcd(x, y):
+    """
+    Calculates the greatest common divisor of two numbers. The first argument must be greater than or equal to the
+    second argument.
+    :param x: the larger number
+    :param y: the smaller number
+    :return: the greatest common divisor
+    """
     assert x >= y
     while y != 0:
         t = y
