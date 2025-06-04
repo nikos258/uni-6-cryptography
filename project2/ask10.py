c0c888143a0400a073fdfd92deac037ca802f9b8
nikos258 <nikar.nikar10@gmail.com>
2025-06-04 11:49:14 +0300

    update ask10

--- project2/ask10.py
+++ project2/ask10.py
@@ -1,8 +1,10 @@
 # 10.20 (153) Pollard rho algorithm
-import egcd, math
 
 
 def f(x, a, b):
+    """
+    A randomizing function to be used by the Pollard rho algorithm.
+    """
     if x < p/3:
         return (g*x) % p, (a+1) % n, b
     elif x < 2*p/3:
@@ -11,29 +13,23 @@ def f(x, a, b):
         return (h*x) % p, a, (b+1) % n
 
 
-# def f(x, a, b):
-#     if x % 3 == 0:
-#         return (x*x) % p, (2*a) % n, (2*b) % n
-#     if x % 3 == 1:
-#         return (g*x) % p, (a+1) % n, b
-#     if x % 3 == 2:
-#         return (h*x) % p, a, (b+1) % n
-
-
-def floyd(x0, m):
+def pollard_rho_disc_log(x0, m):
+    """
+    Implements the Pollard rho algorithm for discrete logarithms. Essentially it uses Floyd's cyclical algorithm to
+    find collisions. Because of the non-deterministic nature the function might lead to failure.
+    :param x0: initialization parameter
+    :param m: number of maximum iterations
+    :return: a tuple with the number of iterations and the two pairs of exponents OR None in case of failure
+    """
     x = x0
     y = x0
-    a1 = b1 = 0
-    a2 = b2 = 0
+    ai = bi = 0
+    aj = bj = 0
     for i in range(1, m):
-        x, a1, b1 = f(x, a1, b1)
-        y, a2, b2 = f(*f(y, a2, b2))
-        # print(i, x, a1, b1, y, a2, b2)
-        first = a1 - a2
-        second = b2 - b1
+        x, ai, bi = f(x, ai, bi)
+        y, aj, bj = f(*f(y, aj, bj))
         if x == y:
-            print(f"first: {first}, second: {second}")
-            return i, a1, b1, a2, b2
+            return i, ai, bi, aj, bj
     return None
 
 
@@ -44,38 +40,19 @@ if __name__ == '__main__':
     h = 2763
 
     x0 = 1  # 1 is not in G2
-    values = floyd(x0, p)
+    values = pollard_rho_disc_log(x0, p)
+    if values is None:
+        print("Failure!")
+        exit()
     i, a1, b1, a2, b2 = values
-    print(values)
 
-    first = a1-a2
-    second = (b2-b1) % n
+    first = (a2-a1) % n
+    second = (b1-b2) % n
 
-    print(f"first: {first}, second: {second}")
-
-    gcd = egcd.egcd(second, n)
-    if first % gcd[0] == 0:
-        inv = egcd.egcd(second, n//gcd[0])
-    print("inverse", inv)
-
-    x = (inv[1] * first) % n
-    print(x)
-    print(pow(g, x, p))  # verification
-
-
-
-    # brute force
-
-    # for x in range(1, p):
-    #     left = second*x % n
-    #     right = first % n
-    #     if left == right:
-    #         print(left, right)
-    #         print("x", x)
-    #         if (g**x) % p == h:
-    #             print("bhdgvldfguaiiirfberh")
-    #             break
-    #         print((g**x) % p, end='\n\n')
-
-
-
+    for x in range(1, p):
+        left = second*x % n
+        right = first % n
+        if left == right:
+            if pow(g, x, p) == h:
+                print("The discrete logarithm of h base g mod p is", x)
+                break
